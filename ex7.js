af5aced425f5fc279d57cf5cb87be0ba920b939a
rafaelconserva <121907511+rafaelconserva@users.noreply.github.com>
2023-09-09 17:44:39 -0300

    Adicionando Q7

---
+++ ex7.js
@@ -0,0 +1,58 @@
+/*Questão 5: Dimitri e Monique foram à feira, cada um comprou e colocou na sua respectiva sacola as frutas que mais gostava. Na sacola de Dimitri tinha: 12 bananas que ele comprou por R$ 2,50; 1 abacaxi que custou 2,78; 5 maçãs que custou 6,94. Já na sacola de Monique tinha 10 bananas que custaram R$ 2,00; 6 tomates que foi R$ 5,00, 2 abacaxi R$ 5,00 e 10 morangos que custaram R$10,00. Implemente um algoritmo que mostre quanto cada um gastou.
+
+Ainda sobre a questão anterior, Dimitri e Monique colocaram as suas sacolas em um único carrinho de compras. Desenvolva um algoritmo que junte as duas sacolas no carrinho, imprima na tela cada um dos produtos contidos no carrinho da seguinte forma Produto: ? Preço: R$ ? Quantidade: ?. E no final apresente o total a ser pago e a quantidade de frutas adicionadas. Obs: Implemente os testes unitários  
+});
+*/
+const dimitriSacola = [
+  { fruta: "banana", preco: 2.5, quantidade: 12 },
+  { fruta: "abacaxi", preco: 2.78, quantidade: 1 },
+  { fruta: "maçã", preco: 6.94, quantidade: 5 }
+];
+
+const moniqueSacola = [
+  { fruta: "banana", preco: 2.0, quantidade: 10 },
+  { fruta: "tomate", preco: 5.0, quantidade: 6 },
+  { fruta: "abacaxi", preco: 5.0, quantidade: 2 },
+  { fruta: "morango", preco: 10.0, quantidade: 10 },
+  { fruta: "laranja", preco: 20.0, quantidade: 10 }
+];
+
+totalDimitri = 0;
+totalMonique = 0;
+
+dimitriSacola.forEach(produto => {
+  totalDimitri += produto.preco * produto.quantidade;
+});
+
+moniqueSacola.forEach(produto => {
+  totalMonique += produto.preco * produto.quantidade;
+});
+
+console.log("Dimitri gastou R$ " + totalDimitri.toFixed(2));
+console.log("Monique gastou R$ " + totalMonique.toFixed(2));
+
+console.log("================= CARRINHO UNIFICADO =================");
+
+const carrinho = dimitriSacola.concat(moniqueSacola);
+
+totalCarrinho = 0;
+quantidadeFrutas = 0;
+
+carrinho.forEach(item => {
+  console.log(
+    "Produto: " + item.fruta + " Preço: R$ " + item.preco.toFixed(2) + " Quantidade: " + item.quantidade
+  );
+  totalCarrinho += item.preco * item.quantidade;
+  quantidadeFrutas += item.quantidade;
+});
+
+console.log("Frutas adicionadas: " + quantidadeFrutas);
+console.log("Total a ser pago: R$ " + totalCarrinho.toFixed(2));
+
+
+
+
+
+
+
+
